d5b7038de53b4f80a352beb0898441d28f5ee93c
akshayrajukuk <a.rajukulkarni@tudelft.nl>
2023-06-19 19:26:03 +0200

    Updated to PEP8

--- GLOBS.py
+++ GLOBS.py
@@ -1,4 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-STORE_DATA_FILE = os.path.join(ROOT_DIR, ('data/stores.json'))
+STORE_DATA_FILE = os.path.join(ROOT_DIR, 'data/stores.json')
